da3e5479ea052e461d080ae307bc37a2916e6851
nur165024 <nur42658@gmail.com>
2021-04-28 12:36:55 +0600

    product done.......

--- index.js
+++ index.js
@@ -2,38 +2,63 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
+var admin = require("firebase-admin");
+require('dotenv').config()
 
-const password = 'IPEGsuSqdeKC0XKe'
-const uri = "mongodb+srv://admin-user-node-mongo-db:IPEGsuSqdeKC0XKe@cluster0.vihvh.mongodb.net/burj-al-arab-db?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vihvh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+
+var serviceAccount = require("./burj-al-arab-e3cc6-firebase-adminsdk-wsbk4-af984725c8.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 client.connect(err => {
-  const bookdbTable = client.db("burj-al-arab-db").collection("book");
-  app.post('/book/store',(req,res) => {
+  const bookdbTable = client.db(`${process.env.DB_NAME}`).collection("book");
+  app.post('/book/store', (req, res) => {
     const newBook = req.body;
     bookdbTable.insertOne(newBook)
-    .then(result => {
+      .then(result => {
         console.log(result);
-    })
+      })
   })
 
-  app.get('/booking',(req,res) => {
-        console.log(req.headers.authorization);
-        const queryEmail = req.query.email
-        bookdbTable.find({email : queryEmail})
-        .toArray((arr,doc) => {
-            res.send(doc);
+  app.get('/booking', (req, res) => {
+    const bearer = req.headers.authorization;
+
+    if (bearer && bearer.startsWith('Bearer ')) {
+      const idToken = bearer.split(' ')[1]
+
+      admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          if (tokenEmail === queryEmail) {
+            bookdbTable.find({ email: queryEmail })
+              .toArray((arr, doc) => {
+                res.send(doc);
+                res.status(200)
+              })
+          }
         })
+        .catch((error) => {
+          res.status(401).send('un-authorized access')
+        })
+    }else{
+      res.status(401).send('un-authorized access')
+    }
   })
 });
 
 
-app.get('/',(req,res) => {
-    res.send('Your server is running!');
+app.get('/', (req, res) => {
+  res.send('Your server is running!');
 })
 
 app.listen(4000);
